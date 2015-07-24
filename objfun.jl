bf64bea9d615730ba0a6ff749fc92c7ed503ed4c
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2015-07-24 15:53:30 -0300

    Create interface for NOMAD

--- objfun.jl
+++ objfun.jl
@@ -6,28 +6,39 @@ max_time = 60
 kmax = 5000
 
 # The arguments needed are η₁, η₂, δ₀, α, β, σ₁ and σ₂
-if length(ARGS) < 7
-  error("ERROR: Need 7 arguments to run: η₁ η₂ δ₀ α β σ₁ σ₃")
+if length(ARGS) < 2
+  return 1e20
 end
 
-args = ["η₁", "η₂", "δ₀", "α", "β", "σ₁", "σ₃"]
+#args = ["η₁", "η₂", "δ₀", "α", "β", "σ₁", "σ₃"]
+#constraint: η₁ ≦ η₂
+args = ["η₁", "η₂", "σ₁", "σ₃"]
+input = open(ARGS[1])
+list = split(readall(input))
+if length(list) != length(args)
+  error("ERROR: Number of variables is wrong")
+end
+values = Float64[parse(x) for x in list]
+close(input)
+
+con = values[1] - values[2]
 
 file = open("test/cutest.param","w")
-for i = 1:7
-  write(file, "$(args[i]) $(ARGS[i])\n")
+for i = 1:length(args)
+  write(file, "$(args[i]) $(values[i])\n")
 end
 write(file, "max_time $max_time\n")
 write(file, "kmax $kmax\n")
 close(file)
 
-test_file = "test/cutest-test.list"
-#test_file = "test/cutest-2var-free.list"
+#test_file = "test/cutest-test.list"
+test_file = "test/cutest-2var-free.list"
 
 objfun = 0.0
 
 file = open(test_file)
 for line in readlines(file)
-  out = readall(`make cutest PROBLEM=$(chomp(line)) OPTS=test/cutest.param`)
+  out = readall(`make cutest-bench PROBLEM=$(chomp(line)) OPTS=test/cutest.param`)
   converged = contains(out, "EXIT 0")
   time = parse(match(r"Elapsed time: ([0-9\.]*)", out).captures[1])
   if converged
@@ -36,4 +47,4 @@ for line in readlines(file)
     objfun += kmax
   end
 end
-println(objfun)
+println("$objfun $con")
